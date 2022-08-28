b701c20578681d0e5db0ff0816f839513914bc17
Kai R <you@example.com>
2022-08-28 14:20:49 +0200

    hello World pattern matching

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,61 @@ fn main() {
     let rom_bytes = load_file();
     for i in (0..rom_bytes.len()).step_by(2) {
         let tmp = &rom_bytes[i..i + 2];
+        let opt = byte_to_opcode(tmp);
+        if !matches!(opt, Opts::NotYetImpl) {
+            println!("{:?}", opt);
+        } else {
+            print!("{:02X?}{:02X?}\n", tmp[0], tmp[1]);
+        }
     }
 }
 
 fn load_file() -> Vec<u8> {
+    use std::env;
     use std::fs;
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
 
-    fs::read("/home/kai/chip-8/roms/helloworld.rom")
-        .expect("Should have been able to read the file")
+    fs::read(file_path).expect("Should have been able to read the file")
 }
 
-fn byte_to_opcode(opt_bytes: &[u8]) {}
+fn byte_to_opcode(opt_bytes: &[u8]) -> Opts {
+    match opt_bytes {
+        [0x00, 0xE0] => Opts::CLS,
+        [0x60..=0x6F, 0x00..=0xFF] => Opts::SetVxToByte {
+            x_v: (opt_bytes[0] - 0x60),
+            byte: opt_bytes[1],
+        },
+        [0xA0..=0xAF, 0x00..=0xFF] => {
+            Opts::SetATo((((opt_bytes[0] - 0xA0) as u16) << 8) | (opt_bytes[1] as u16))
+        }
+        [0xF0..=0xFF, 0x0A] => Opts::WaitForKeyStoreInVx(opt_bytes[0] - 0xF0),
+        [0xF0..=0xFF, 0x18] => Opts::SetSoundTimerToVx(opt_bytes[0] - 0xF0),
+        [0xF0..=0xFF, 0x29] => Opts::SetIToLocationOfSpriteDigitVx(opt_bytes[0] - 0xF0),
+        [0xD0..=0xDF, 0x00..=0xFF] => Opts::DisplayNByteSpriteFromIAtXY {
+            x: (opt_bytes[0] - 0xD0),
+            y: ((opt_bytes[1] & 0xF0) >> 4),
+            n: (opt_bytes[1] & 0x0F),
+        },
+        [0x10..=0x1F, 0x00..=0xFF] => Opts::SetPC((((opt_bytes[0] - 0x10) as u16) << 8) | (opt_bytes[1] as u16)),
+        _ => Opts::NotYetImpl,
+    }
+}
 
 #[derive(Debug)]
 enum Opts {
     CLS,
+    SetVxToByte { x_v: u8, byte: u8 },
+    SetATo(u16),
+    WaitForKeyStoreInVx(u8),
+    SetSoundTimerToVx(u8),
+    SetIToLocationOfSpriteDigitVx(u8),
+    DisplayNByteSpriteFromIAtXY { x: u8, y: u8, n: u8 },
+    SetPC(u16),
+    NotYetImpl,
 }
+
 // 0NNN 	Call
-// 00E0 	Display
 // 00EE 	Flow
 // 1NNN 	Flow
 // 2NNN 	Flow
